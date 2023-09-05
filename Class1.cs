08a5a9e3e3f4678cdeb32614f02e37c73ce2d8cd
FireDemon2903 <jona015v@edu.rybners.dk>
2023-09-05 12:10:23 +0200

    first commit

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Diary
+{
+    // Documentation: https://learn.microsoft.com/en-us/azure/data-lake-analytics/data-lake-analytics-u-sql-get-started
+    internal class DiaryIO
+    {
+        public static void Write(string text)
+        {
+            //
+        }
+        public static string Read(string text)
+        {
+            // 
+        }
+    }
+}
